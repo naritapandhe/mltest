f0d1ec2c619401e4e9dccbd0f1e64574c4535db2
Narita Pandhe <narita.pandhe@gmail.com>
2016-07-29 15:29:33 -0400

    New changes

--- Sample6.py
+++ Sample6.py
@@ -14,40 +14,45 @@ from sklearn.metrics import classification_report
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import cross_validation
 from sklearn.cross_validation import KFold
+from sklearn import cross_validation
 
-df=pandas.read_json("updatedFinalData.json")
+df=pandas.read_json("updatedSubset.json")
 
 
 X = (df[["identifier"]])
 X_data = np.array(X["identifier"])
+#print len(X_data)
 #print X_train
 #print X_train.shape
 
-Y = np.array(df[["gadget_id"]])
-print Y
-print Y.shape
+Y = (df[["labels"]])
+Y_data = Y["labels"]
+#print Y_data
+#print Y.shape
+
 
 #for x in X_train:
 #	print x
 lb = preprocessing.MultiLabelBinarizer()
-Y_data_transformed = lb.fit_transform(Y)
-print Y_data_transformed
+Y_data_transformed = lb.fit_transform(Y_data)
+#print Y_data_transformed
 
-kf = KFold(len(X_data), n_folds=10)
+
+"""kf = KFold(len(X_data), n_folds=10)
 for train_index, test_index in kf:
     print("TRAIN:", train_index, "TEST:", test_index)
     X_train, X_test = X_data[train_index], X_data[test_index]
-    y_train, y_test = Y_data_transformed[train_index], Y_data_transformed[test_index]
+    y_train, y_test = Y_data_transformed[train_index], Y_data_transformed[test_index]"""
 
-#X_train, X_test, y_train, y_test = sklearn.cross_validation.KFold(n=4, n_folds=2, shuffle=False,random_state=None)
-#train_test_split(X_data, Y_data_transformed, test_size=0.2, random_state=5)
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X_data, Y_data_transformed, test_size=0.2, random_state=5)
 
 
 
 classifier = Pipeline([
     ('vectorizer', CountVectorizer()),
     ('tfidf', TfidfTransformer()),
-    ('clf', OneVsRestClassifier(LinearSVC(C=10.)))])
+    ('clf', (KNeighborsClassifier(n_neighbors=3)))])
+    #('clf', OneVsRestClassifier(LinearSVC(C=1.)))])
 
 classifier.fit(X_train, y_train)
 
@@ -55,11 +60,38 @@ predicted = classifier.predict(X_test)
 all_labels = lb.inverse_transform(predicted)
 
 #print all_labels
+count=0
 for item, labels in zip(X_test, all_labels):
     print (item)
     print labels
+    count +=1
 
 
+print (count)
+print (classifier.score(X_train,y_train))  
+print (classifier.score(X_test,y_test))    
+
+
+
+classifier = Pipeline([
+    ('vectorizer', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+   ('clf', OneVsRestClassifier(LinearSVC(C=1.)))])
+
+classifier.fit(X_train, y_train)
+
+predicted = classifier.predict(X_test)
+all_labels = lb.inverse_transform(predicted)
+
+#print all_labels
+count=0
+for item, labels in zip(X_test, all_labels):
+    print (item)
+    print labels
+    count +=1
+
+
+print (count)
 print (classifier.score(X_train,y_train))  
 print (classifier.score(X_test,y_test))    
 
